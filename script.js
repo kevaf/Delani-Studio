2f112dc5b62601c6a4ea3f37373736c90081240a
kevaf <kevinkipyegon20@gmail.com>
2021-07-24 18:31:41 +0300

    improved what we do page

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ $(document).ready (function(){
     });
     
 });
+
+$(document).ready (function(){
+    $("#devel").click(function(){
+        $('#develtxt').toggle();
+        $('#devel').hide();
+    });
+    
+});
+$(document).ready (function(){
+    $("#develtxt").click(function(){
+        $('#devel').toggle();
+        $('#develtxt').hide();
+    });
+    
+});
+
+$(document).ready (function(){
+    $("#product").click(function(){
+        $('#producttxt').toggle();
+        $('#product').hide();
+    });
+    
+});
+$(document).ready (function(){
+    $("#producttxt").click(function(){
+        $('#product').toggle();
+        $('#producttxt').hide();
+    });
+    
+});
