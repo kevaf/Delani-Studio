5567f381547349580bee689d47a47be91766ae77
kevaf <kevinkipyegon20@gmail.com>
2021-07-24 11:40:36 +0300

    added toogle effect to design text

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-$()
+$(document).ready (function(){
+    $("#design").click(function(){
+        $('#destxt').toggle();
+        $('#design').hide();
+    });
+    
+});
+$(document).ready (function(){
+    $("#destxt").click(function(){
+        $('#design').toggle();
+        $('#destxt').hide();
+    });
+    
+});
