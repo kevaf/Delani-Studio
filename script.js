32d6d8028a5235a98ab5902c3cc6a37cc25d0191
kevaf <kevinkipyegon20@gmail.com>
2021-07-25 13:38:19 +0300

    added hover effect to projects

--- script.js
+++ script.js
@@ -1,44 +1,73 @@
 $(document).ready (function(){
+   
+//    WHAT WE DO 
+
     $("#design").click(function(){
         $('#destxt').toggle();
         $('#design').hide();
     });
-    
-});
-$(document).ready (function(){
+
     $("#destxt").click(function(){
         $('#design').toggle();
         $('#destxt').hide();
     });
     
-});
-
-$(document).ready (function(){
     $("#devel").click(function(){
         $('#develtxt').toggle();
         $('#devel').hide();
     });
-    
-});
-$(document).ready (function(){
+
     $("#develtxt").click(function(){
         $('#devel').toggle();
         $('#develtxt').hide();
     });
     
-});
-
-$(document).ready (function(){
     $("#product").click(function(){
         $('#producttxt').toggle();
         $('#product').hide();
     });
     
-});
-$(document).ready (function(){
     $("#producttxt").click(function(){
         $('#product').toggle();
         $('#producttxt').hide();
     });
-    
+  
+
+
+// PORTFOLIO HOVER
+
+    $("#project1").mouseover(function(){
+        $(this).css('opacity', '0.4');
+        $('#p1').show();
+    });
+    $("#project1").mouseout(function(){
+        $(this).css('opacity', '1');
+        $('#p1').hide();
+    });
+
+    $("#project2").mouseover(function(){
+        $(this).css('opacity', '0.4');
+        $('#p2').show();
+    });
+    $("#project2").mouseout(function(){
+        $(this).css('opacity', '1');
+        $('#p2').hide();
+    });
+    $("#project3").mouseover(function(){
+        $(this).css('opacity', '0.4');
+        $('#p3').show();
+    });
+    $("#project3").mouseout(function(){
+        $(this).css('opacity', '1');
+        $('#p3').hide();
+    });
+    $("#project4").mouseover(function(){
+        $(this).css('opacity', '0.4');
+        $('#p4').show();
+    });
+
+    $("#project4").mouseout(function(){
+        $(this).css('opacity', '1');
+        $('#p4').hide();
+    });
 });
